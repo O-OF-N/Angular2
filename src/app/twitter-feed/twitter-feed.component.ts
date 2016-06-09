21b6159426a1ebd553586e493a56f16766d73fce
vinodm1986 <vinodm1986@gmail.com>
2016-06-09 01:59:30 -0400

    Added twitter component

--- src/app/twitter-feed/twitter-feed.component.ts
+++ src/app/twitter-feed/twitter-feed.component.ts
@@ -4,13 +4,21 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'app-twitter-feed',
   templateUrl: 'twitter-feed.component.html',
-  styleUrls: ['twitter-feed.component.css']
+  styles: ['twitter-feed.component.css']
 })
 export class TwitterFeedComponent implements OnInit {
-
-  constructor() {}
-
+  url: string = "https://twitter.com/vmisblue";
   ngOnInit() {
+    (!function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    } (document, "script", "twitter-wjs"));
+
   }
 
 }
