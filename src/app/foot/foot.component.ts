a58df4878f211880181869c61f997244ce3e3633
vinodm1986 <vinodm1986@gmail.com>
2016-06-09 10:46:16 -0400

    Loading image

--- src/app/foot/foot.component.ts
+++ src/app/foot/foot.component.ts
@@ -22,6 +22,15 @@ export class FootComponent implements OnInit {
       js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=1687445188183815";
       fjs.parentNode.insertBefore(js, fjs);
     } (document, 'script', 'facebook-jssdk'));
+
+    !function(d,s,id) {
+      var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+      if(!d.getElementById(id)){
+        js=d.createElement(s);
+        js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js,fjs);
+      }
+    }(document, 'script', 'twitter-wjs');
   }
 
 }
