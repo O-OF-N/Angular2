80ad9d1b4bc0c1877270c3cf4ad64a074be24ec0
vinodm1986 <vinodm1986@gmail.com>
2016-06-09 08:18:53 -0400

    twit

--- src/app/foot/foot.component.ts
+++ src/app/foot/foot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,13 +6,22 @@ import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
   templateUrl: 'foot.component.html',
   styleUrls: ['foot.component.css']
 })
-export class FootComponent {
-  @Input() copyRight:String;
-  @Output('emitter') outPut:EventEmitter<String> = new EventEmitter();
-  constructor() {}
-  onClick(){
+export class FootComponent implements OnInit {
+  @Input() copyRight: String;
+  @Output('emitter') outPut: EventEmitter<String> = new EventEmitter();
+  constructor() { }
+  onClick() {
     console.log('clicked');
     this.outPut.emit('lll');
+  }
+  ngOnInit() {
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=1687445188183815";
+      fjs.parentNode.insertBefore(js, fjs);
+    } (document, 'script', 'facebook-jssdk'));
   }
 
 }
