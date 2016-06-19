616e5c430f349159493d9929c245f6042ae50de1
vinodm1986 <vinodm1986@gmail.com>
2016-06-19 10:27:48 -0400

    animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import {DataService} from '../../services/data-service';
   moduleId: module.id,
   selector: 'app-home',
   templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css']
+  styleUrls: ['home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
