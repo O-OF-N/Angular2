797be38b5a4149fea82553fde372c9e8781f112b
vinodm1986 <vinodm1986@gmail.com>
2016-06-21 17:30:27 -0400

    css changes

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -84,7 +84,7 @@ export class Data implements DataComponent {
     headerComponent: HeaderComponent = {
         //The ask us global logo image path
         imagePath: "logo.jpg",
-        menuPath: "Menu.png",
+        menuPath: "menu.png",
         //This is complex!! Better not to mess around with this.
         //Contact Vinod Mohanan if any changes are needed with routes.
         routeComponent: [{
